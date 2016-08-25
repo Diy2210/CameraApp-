bcc331518b234dc9e3594c0e8fe5dd2a253577fd
Diy2210 <Diy2210@mac-mini-diy2210.local>
2016-08-25 11:56:59 +0300

    Initial Commit

---
+++ CameraTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CameraTest
+//
+//  Created by Diy2210 on 25.08.16.
+//  Copyright © 2016 Diy2210. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
